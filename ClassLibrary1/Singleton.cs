e4b00316bcd6365228f43344abd79488af0b7e61
Zuzi <zunils@gmail.com>
2023-06-28 01:06:30 +0530

    First

---
+++ ClassLibrary1/Singleton.cs
@@ -0,0 +1,26 @@
+﻿namespace Oops
+{
+    public sealed class SingleTonObj
+    {
+        private static readonly SingleTonObj instance = new SingleTonObj();
+
+        static SingleTonObj() {
+
+            Console.WriteLine("static");
+
+        }
+        
+        private SingleTonObj() { Console.WriteLine("private"); }
+
+        public static SingleTonObj Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+
+
+ 
+}
---
+++ DataStructureAlgo/Algorithms.cs
@@ -0,0 +1,45 @@
+﻿namespace DataStructureAlgo
+{
+    public class Algorithms
+    {
+
+        private int[] itemList = new int[10];
+
+        public Algorithms()
+        {
+            itemList = new int[10] { 2, 4, 6, 7, 14, 44, 88, 155, 753, 4436 };
+        }
+
+        public int? binarySearch(int searchQ, out int numberOfTries)
+        {
+            int? foundIndex = null;
+            int tries = 0;
+
+            int lowIndex = 0;
+            int highIndex = itemList.Length - 1;
+            int midIndex = (lowIndex + highIndex) / 2;
+
+            while (lowIndex <= highIndex)
+            {
+                ++tries;
+                if (searchQ == itemList[midIndex])
+                {
+                    foundIndex = midIndex;
+                    break;
+                }
+                else if (searchQ < itemList[midIndex])
+                    highIndex = midIndex - 1;
+                else if (searchQ > itemList[midIndex])
+                    lowIndex = midIndex + 1;
+
+                midIndex = (lowIndex + highIndex) / 2;
+            }
+
+            numberOfTries = tries;
+
+            return foundIndex;
+
+        }
+
+    }
+}
